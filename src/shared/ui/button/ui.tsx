d90f07dc37ad1a08d348a89e5928c61344706d33
Danil Efremov <danileph@bk.ru>
2023-08-21 16:21:46 +0300

    Button UI has been updated.

--- src/shared/ui/button/ui.tsx
+++ src/shared/ui/button/ui.tsx
@@ -7,8 +7,8 @@ interface IButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   size?: "small" | "medium" | "large";
 }
 
-const styles = {
-  base: "border-darkest border-2 rounded-[8px] px-4 py-2 shadow-csm uppercase select-none text-sm font-semibold tracking-widest hover:backdrop-brightness-95 active:translate-y-[3px] active:shadow-none",
+const styles = /* @tw */ {
+  base: "border-darkest border-2 rounded-[8px] px-4 py-2 shadow-csm uppercase select-none text-sm font-semibold tracking-widest hover:backdrop-brightness-95 translate-y-[-3px] active:translate-y-[0px] active:shadow-none ",
   variants: {
     primary: "bg-primary hover:brightness-110",
     secondary: "",
@@ -18,7 +18,7 @@ const styles = {
       "py-1.5 px-3 text-xs rounded-[7px] shadow-csm-sm border-[2px] active:translate-y-[2px]",
     medium: "",
     large:
-      "py-2.5 px-6 rounded-[9px] text-md shadow-csm-lg active:translate-y-[4px] border-[2.5px]",
+      "py-2.5 px-6 rounded-[9px] text-md shadow-csm-lg translate-y-[-4px]  active:translate-y-[0px] border-[2.5px]",
   },
 };
 
