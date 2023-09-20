8c5e2d3ce84161aedfcd86da26cb6a8cb15ea104
Danil Efremov <danileph@bk.ru>
2023-09-20 14:41:09 +0300

    Some small updates

--- src/widgets/sidebar/ui/menu/config.tsx
+++ src/widgets/sidebar/ui/menu/config.tsx
@@ -2,7 +2,7 @@ import { MenuItem } from "@/widgets/sidebar";
 import { BookIcon, PersonIcon, SearchIcon } from "@/shared/ui/icons";
 
 export const menu: MenuItem[] = [
-  { name: "Searching", url: "/searching", renderIcon: SearchIcon },
+  { name: "Search", url: "/search", renderIcon: SearchIcon },
   { name: "Vocabulary", url: "/vocabulary", renderIcon: BookIcon },
   { name: "Profile", url: "/profile", renderIcon: PersonIcon },
 ];
