4b33e45a0d1351a9ab11b8465250a62f513732fe
Danil Efremov <danileph@bk.ru>
2023-09-21 13:35:23 +0300

    Last searched component has been created

---
+++ src/widgets/last-searched/ui.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+
+interface ILastShearchedProps extends React.HTMLAttributes<HTMLElement> {}
+
+export const LastShearched: FC<ILastShearchedProps> = () => {
+  return <div></div>;
+};
